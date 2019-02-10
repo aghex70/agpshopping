4bee4803981fe73ef1e10931a5705a668045e0dc
Alberto <aghex70@gmail.com>
2019-02-10 16:45:02 +0100

    Functional test that fills interest and checks the result

--- shopping/functional_tests.py
+++ shopping/functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewCustomerTest(unittest.TestCase):
@@ -13,8 +15,42 @@ class NewCustomerTest(unittest.TestCase):
         # She goes to check out this new app she heard by going to its homepage
         self.browser.get('http://localhost:8000')
 
-        # She notices that she is on the interests fill in page
-        self.assertIn('Customer interests', self.browser.title)
+        # She notices that she is on her index page
+        self.assertIn('Personal index', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text()
+        self.assertIn('Please select your interests', header_text)
+
+        # She is required to fill in her interests
+        cryptocurrency_checkbox = self.browser.find_element_by_id('id_cryptocurrency')
+        book_checkbox = self.browser.find_element_by_id('id_books')
+
+        # She checks "cryptocurrencies"
+        cryptocurrency_checkbox.click()
+        self.assertIs(cryptocurrency_checkbox.get_attribute('checked'), True)
+
+        # She also checks "books"
+        book_checkbox.click()
+        self.assertIs(book_checkbox.get_attribute('checked'), True)
+
+        # When she confirms her interests, the page updates, and now the page shows
+        # her interests like a list : "Cryptocurrencies", "Books"
+        time.sleep(1)
+        current_interests_list = self.browser.find_element_by_tag_name('ul').text()
+        self.assertIn('Currently selected interests', current_interests_list)
+        interests = current_interests_list.find_elements_by_tag_name('li')
+
+        self.assertTrue(
+            any(row.text == 'Cryptocurrencies' for interest in interests))
+
+        self.assertTrue(
+            any(row.text == 'Books' for interest in interests))
+
+        self.fail('Test has finished')
+
+
+
+
+
         self.fail('Finishing the test')
 
 if __name__ == '__main__':
